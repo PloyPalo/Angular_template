66b1bd98234cfc6008f3bc2dc499da555776df39
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2023-01-24 15:11:09 +0700

    edit header, footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,13 +5,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+     <span class="copyright">Copyright © 2023 :</span> 
+     <span>&nbsp;<a href="https://fortwachirawuthospital.rta.mi.th/homepage/">Fort Wachirawut Hospital</a> </span>  
+      <!-- Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019 -->
     </span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
+      <span class="copyright">Smart Dashboard 1.0</span>
+      <!-- <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
       <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="#" target="_blank" class="ion ion-social-linkedin"></a> -->
     </div>
   `,
 })
